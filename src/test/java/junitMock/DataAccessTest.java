232b1a68aaecf8782c7d0eb92f0ef72a2498197a
Aitor <aitor.ortega.98@gmail.com>
2020-10-14 15:19:27 +0200

    what

--- src/test/java/junitMock/DataAccessTest.java
+++ src/test/java/junitMock/DataAccessTest.java
@@ -24,6 +24,8 @@ import exceptions.QuestionAlreadyExist;
 import test.businessLogic.TestFacadeImplementation;
 
 class DataAccessTest {
+	//Aitor
+	
 	// sut- System Under Test
 	private DataAccess sut = new DataAccess(ConfigXML.getInstance().getDataBaseOpenMode().equals("initialize"));;
 	private TestFacadeImplementation testBL = new TestFacadeImplementation();
